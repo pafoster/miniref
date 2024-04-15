a1d227050596b96ef3e263aa99a64823f2ab45cc
Peter Foster <pafoster@users.noreply.github.com>
2024-04-15 22:21:55 +0100

    Switch to vertical positioning

--- scripts/refsearch.sh
+++ scripts/refsearch.sh
@@ -7,6 +7,7 @@ MINIREF_HOME=${MINIREF_HOME:-${HOME}/miniref}
 
 cd "$MINIREF_HOME" && find . -type d -exec ref_summarise.sh {} \; |
  	fzf --read0 --reverse --exact --no-hscroll \
+    --preview-window down:80% \
     --preview 'ref_preview.sh {1}' \
     --bind 'f1:execute(refview.sh {1})' \
     --bind 'enter:execute(cd {1} && $SHELL)' \
